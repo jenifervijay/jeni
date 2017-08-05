784a7d036384d7884dede9f0338e0549ba0003bc
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2017-08-05 16:35:16 +0530

    Create ev.c

---
+++ ev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the num value:");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("the given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+}
