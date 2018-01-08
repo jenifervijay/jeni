1925f292638f20d9edc37d4d2a51eb6abb6f3979
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-08 16:30:31 +0530

    Create he.java

---
+++ he.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class he
+{
+public static void main(String args[]);
+{
+int a;
+Scanner s=new Scanner(Systen.in);
+System.out.println("how many times you want to print hello");
+a=s.newInt();
+for(i=0;i<=a;i++)
+{
+System.out.println("hello");
+}
+}
+}
