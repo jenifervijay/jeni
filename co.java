360b5928a910452b0fc91b3f5c85d984396e1c21
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-08 16:46:28 +0530

    Create co.java

---
+++ co.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class co
+{
+public static void main(String args[])
+{
+int a,i=0;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the no:");
+a=s.netInt();
+while(a>0)
+{
+n=n/10;
+i++;
+}
+System.out.println("count of the given no is:"+i);
+}
+}
+
