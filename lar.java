94ba627c5ab79eb400f3179cfc5d64d07d569698
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-08 16:21:51 +0530

    lar.java
    good

---
+++ lar.java
@@ -0,0 +1,21 @@
+class lar
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(agrs[1]);
+int c=Integer.parseInt(args[2]);
+if((a>b) && (a>c))
+{
+System.out.println("a is greater:"+a);
+}
+else if((b>a) && (b>c))
+{
+System.out.println("b is greater:"+b);
+}
+else
+{
+System.out.println("c is greater:"+c);
+}
+}
+}
