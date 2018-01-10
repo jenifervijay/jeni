f0320e0863a82494f3766ec5dec15f9699bda852
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-01-10 17:00:28 +0530

    Create po.java

---
+++ po.java
@@ -0,0 +1,19 @@
+import java.util.java*;
+class po
+{
+public static void main(String agrs[])
+{
+Scanner s=new Scanner(System.in);
+int base,power,i,no=1;
+System.out.println("enter the base value:");
+base=s.nextInt();
+System.out.pribntln("enter the power value:);
+power=a.nextInt();
+for(i=0;i<power;i++)
+{
+no=no*base;
+}
+System.out.println(no);
+}
+}
+
