a10a81db051372f31182841ed91bfdea6c89ac01
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-05 16:19:48 +0530

    Create str6.c

---
+++ str6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int str[50],countspe,count;
+clrscr();
+printf("enter the string:");
+scanf("%s",&str[count]);
+for(count=0;str[count]!='\0';count++)
+{
+if(str[count]==' ')
+countspe++;
+}
+printf(" special characters are:%d",countspe);
+getch();
+}
