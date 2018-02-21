2291b2a1f8d7b0a6d0c264b9ce1948458a328361
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-21 16:47:37 +0530

    Create mul13.cpp

---
+++ mul13.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+if(n%13==0)
+{
+cout<<"multiple of 13";
+}
+else
+{
+cout<<"not a multiple of 13";
+}
+getch();
+}
