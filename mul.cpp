95510421d21bed495691d3e2d50c65cfa5132f8e
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-21 15:38:34 +0530

    Create mul.cpp

---
+++ mul.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+cout<<"enter the number:";
+cin>>n;
+if(n%7==0)
+{
+cout<<"multiple of 7";
+}
+else
+{
+cout<<"not a multiple of 7";
+}
+getch();
+}
