7e9eca74fcc238e6643bdd27f0815039bc92bb80
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-02-28 16:09:15 +0530

    Create com.cpp

---
+++ com.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,temp=0,i;
+clrscr();
+cout<<"enter the number:";
+cin>>n;
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+temp=1;
+break;
+}
+}
+if(temp==0)
+{
+cout<<" not a composite number";
+}
+else
+{
+cout<<"composite number";
+}
+getch();
+}
