2826523deb423bd99e5f85f3c282d8b6e022e113
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-02 16:33:42 +0530

    Create dif.cpp

---
+++ dif.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,n3,n4,n5,n6;
+clrscr();
+cout<<"\n enter the two numbers in 1st time:";
+cin>>n1>>n2;
+cout<<"\n enter the two numbers in 2st time:";
+cin>>n3>>n4;
+cout<<"\n enter the two numbers in 3st time:";
+cin>>n5>>n6;
+n1=n1-n2;
+n3=n3-n4;
+n5=n5-n6;
+cout<<"differences are:\n";
+cout<<"\n"<<n1;
+cout<<"\n"<<n3;
+cout<<"\n"<<n5;
+getch();
+}
