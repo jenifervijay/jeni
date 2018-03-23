a63eaaa1e1a740e9f49a415ec618651830250b4f
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-23 16:07:17 +0530

    Create kk.c

---
+++ kk.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int n,i,res,a[20],t=0;
+char r[10];
+clrscr();
+printf("enter roman number:");
+scanf("%s",r);
+n=strlen(r);
+for(i=0;i<n;i++)
+{
+if(r[i]=='I')
+a[i]=1;
+else if(r[i]=='V')
+a[i]=5;
+else if(r[i]=='X')
+a[i]=10;
+else if(r[i]=='L')
+a[i]=50;
+else if(r[i]=='C')
+a[i]=100;
+else if(r[i]=='D')
+a[i]=500;
+else
+printf("\nINVALID");
+}
+for(i=0;i<n;i+=2)
+{
+if(a[i]<a[i+1])
+	{
+		res=a[i+1]-a[i];
+	}
+	else
+	{
+	res=a[i]+a[i+1];
+	}
+t=res+t;
+}
+printf("\n%d",t);
+getch();
+}
