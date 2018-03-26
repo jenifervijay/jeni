197be68dcae35fa57cd6d876b3b52917b920def9
jenifervijay <30748334+jenifervijay@users.noreply.github.com>
2018-03-26 16:07:37 +0530

    Create ho.cpp

---
+++ ho.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void  main()
+{
+char s[50];
+cout<<"\n enter the day:";
+cin>>s; 
+if(s[0]=='s'||s[0]=='S')
+ {
+  
+  cout<<"\n holiday";}
+    else
+    {
+      cout<<"\n working day";
+    }
+    getch();
+}
